442cabab94e5892864e2a01cffa7c60ecab35467
Erin <erin.s.enriquez@gmail.com>
2023-07-31 11:37:46 -0400

    Initial Commit

---
+++ zenZone/zenZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  zenZoneApp.swift
+//  zenZone
+//
+//  Created by Scholar on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct zenZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
